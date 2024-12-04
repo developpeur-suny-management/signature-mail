e2e6717970ddc839b3d1f05cef4495e9c8446f2c
developpeur-suny-management <developpeur@sunymanagement.com>
2024-12-04 16:38:50 +0100

    Update profileForm.tsx

--- components/profileForm.tsx
+++ components/profileForm.tsx
@@ -1,4 +1,4 @@
-"use client";
+import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -12,6 +12,8 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
 import { formSchema } from "@/components/profileFormSchema";
 
 type ProfileFormProps = {
@@ -19,22 +21,46 @@ type ProfileFormProps = {
 };
 
 export function ProfileForm({ onSubmit }: ProfileFormProps) {
+  const defaultValues = {
+    name: "",
+    job: "",
+    phoneNumber: "",
+    email: "",
+    address: "70, Rue de Ponthieu, 75008 Paris",
+    url: "http://www.sunymanagement.com",
+    message: undefined, // Le message n'est pas défini par défaut
+  };
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      job: "",
-      phoneNumber: "",
-      email: "",
-      address: "70, Rue de Ponthieu, 75008 Paris",
-      url: "http://www.sunymanagement.com",
-    },
+    defaultValues,
     mode: "onChange",
   });
 
+  const [showMessage, setShowMessage] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    // Retirer le champ message s'il n'est pas activé
+    if (!showMessage) {
+      delete data.message;
+    }
+    setIsSubmitted(true); // Marquer comme soumis
+    onSubmit(data);
+  };
+
+  const handleReset = () => {
+    setShowMessage(false); // Réinitialise aussi l'état de la Switch
+    form.reset(defaultValues); // Réinitialise les valeurs par défaut
+    setIsSubmitted(false); // Réinitialise l'état de soumission
+  };
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-8 w-full"
+      >
         <FormField
           control={form.control}
           name="name"
@@ -125,7 +151,44 @@ export function ProfileForm({ onSubmit }: ProfileFormProps) {
           )}
         />
 
-        <Button type="submit">Crée signature mail</Button>
+        <div className="flex items-center space-x-2">
+          <Switch
+            id="as-message"
+            checked={showMessage}
+            onCheckedChange={() => setShowMessage(!showMessage)}
+          />
+          <Label htmlFor="as-message">Ajouter un message personnalisé</Label>
+        </div>
+
+        {showMessage && (
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message personnalisé</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Entrez votre message personnalisé"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        )}
+        <div className="flex gap-4">
+          <Button variant="outline" type="button" onClick={handleReset}>
+            Effacer le formulaire
+          </Button>
+          <Button type="submit">Créer signature mail</Button>
+        </div>
+        {isSubmitted && (
+          <div className="formResult">
+            <p>Votre formulaire a été soumis avec succès !</p>
+          </div>
+        )}
       </form>
     </Form>
   );
