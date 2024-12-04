e80cf1cef366cd864bd51da7d3df66071298ac95
developpeur-suny-management <developpeur@sunymanagement.com>
2024-12-04 16:39:40 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,15 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="fr"
-      className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-blue-50 to-white"
-    >
+    <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-blue-50 to-white`}
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
